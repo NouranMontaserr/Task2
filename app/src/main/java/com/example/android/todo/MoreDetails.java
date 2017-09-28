ee14c64eb0bf88b136270215ceb4ea0b0ed21051
Nouran Montaser <nouran@hub.gov>
2017-09-28 20:24:51 +0200

    first commit

---
+++ app/src/main/java/com/example/android/todo/MoreDetails.java
@@ -0,0 +1,9 @@
+package com.example.android.todo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MoreDetails extends AppCompatActivity{
+}
